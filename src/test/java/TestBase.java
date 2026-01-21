2e2e4e18516e92162e3d4164c040e52ca698bc28
KonushkinMaksim <mkonushkinqa@gmail.com>
2026-01-21 14:14:39 +0400

    added base remote driver config(2)

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -6,8 +6,8 @@ public class TestBase {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 1000; // default 4000
+        //Configuration.pageLoadStrategy = "eager";
+        //Configuration.timeout = 1000; // default 4000
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
