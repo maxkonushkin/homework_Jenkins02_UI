7acdd23aab0bf45fc3cad270faf4251a7d8a7b42
KonushkinMaksim <mkonushkinqa@gmail.com>
2026-01-21 18:16:49 +0400

    added allure-selenide

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
