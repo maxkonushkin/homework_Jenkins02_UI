f89c3fad7fd09c516a6aba2b4fee53638938e605
KonushkinMaksim <mkonushkinqa@gmail.com>
2026-01-21 18:28:24 +0400

    added fixedban

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -18,8 +18,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         // для борьбы с баннерами
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
+
         Configuration.pageLoadStrategy = "eager";
         //Configuration.timeout = 1000; // default 4000
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
