c464eb84a191a30f7bc4cb1a9ccb3c48e4889ff5
KonushkinMaksim <mkonushkinqa@gmail.com>
2026-01-26 15:52:02 +0400

    added clone homework

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -20,6 +20,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
